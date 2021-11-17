c2f14dcf9d3f711eba51d601244a03bbc8cea5b5
Nozibul <nozibulislamspi@gmail,com>
2021-11-17 23:21:31 +0600

    update

--- index.js
+++ index.js
@@ -66,8 +66,10 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
     // delete all manage all order
     app.delete('/orders/:id', async(req, res)=>{
         const id = req.params.id;
-        const query = { _id: ObjectId(id)};
+        console.log(id)
+        const query = {_id:ObjectId(id)};
         const result = await orderPackage.deleteOne(query);
+        console.log(result)
         res.json(result)
     }) 
 
